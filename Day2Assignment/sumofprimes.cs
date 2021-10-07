e1a7350f70fe4e366f9de3f353fff76eb7e32bc1
SriPrathyushaLakkaraju <48018141+SriPrathyushaLakkaraju@users.noreply.github.com>
2021-10-07 21:56:36 +0530

    Add files via upload

---
+++ Day2Assignment/sumofprimes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SampleConApp.Day02
+{
+    class sumofprimes
+    {
+        public static int SumOfPrimes(int from, int to)
+        {
+            int sum = 0,nsum=0,K=0;
+            for(int i=from;i<=to;i++)
+            {
+                int c = 0;
+                for(int j=1;j<=100;j++)
+                {
+                    if (i % j == 0)
+                    {
+                        c++;
+                    }
+                    else
+                        K++;
+                }
+                if ( c == 2)
+                    sum = sum + i;
+                else
+                    nsum = nsum + from;
+                    
+            }
+       
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter from and to numbbers");
+            int f = int.Parse(Console.ReadLine());
+            int t = int.Parse(Console.ReadLine());
+            int res=SumOfPrimes(f, t);
+              Console.WriteLine($"Sum of primes between {f} and {t} is {res}");
+        }
+    }
+}
